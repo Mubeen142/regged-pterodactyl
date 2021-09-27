e53e518a1224d6fd4547ef57bf32e1be9de286de
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2021-09-27 20:29:08 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+**/ Regged Theme for Pterodactyl \**
+**/ Theme by Mubeen for Pterodactyl 1.6.2 \**
+
+Before proceeding with installation make sure you have checked the following things:
+
+* Make sure you're running the latest version of Pterodactyl
+* Make sure you don't have any other theme installed
+* Make sure you don't use WinsCP to upload the files, use FileZilla Instead
+
+Installation:
+1. upload the files inside the pterodactyl folder to /var/www/pterodactyl
+2. Build Pterodactyl Panel Assets, Tutorial: https://pterodactyl.io/community/customization/panel.html
+3. You're done!
+
+
+Need help? Join our discord: https://discord.gg/RJfCxC2W3e
---
+++ pterodactyl/config/themes/regged.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Regged Theme Settings - mubeen#0001
+    |--------------------------------------------------------------------------
+    | Regged is a very simple and basic Pterodactyl theme.
+    | If you need help, join our discord server: https://discord.gg/RJfCxC2W3e
+    */
+
+    'author' => 'Mubeen',
+    'version' => '1.0.0',
+        
+    /*
+    |--------------------------------------------------------------------------
+    | RGB Settings
+    |--------------------------------------------------------------------------
+    */
+
+    'rgb' => [
+        'enable_panel_page' => 'true',
+        'enable_login_page' => 'true',
+        
+        'color_1' => '#45a3e5',
+        'color_2' => '#66bf39',
+        'color_3' => '#eb670f',
+        'color_4' => '#f35',
+        'color_5' => '#864cbf',
+
+    ],
+
+
+];
